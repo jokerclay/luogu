618645d9dfc11b9002c0033461be0c3c710788cc
jokerclay <20792095472@qq.com>
2023-07-27 17:45:25 +0800

    first commit

---
+++ 【入门1】顺序结构/输出字符菱形/main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    int n = 3;
+
+    // Upper part of the pattern
+    for (int i = 1; i <= n; i++) {
+
+
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    // Lower part of the pattern
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= 2 * i - 1; k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+
+#if 0 
+    int n = 3;
+    int spaces = n - 1;
+    int stars = 1;
+
+    printf("debugging....\n");
+
+    for (int i = 1; i <= 2 * n - 1; i++) {
+
+        for (int j = 1; j <= spaces; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= stars; k++) {
+            printf("*");
+        }
+        printf("\n");
+
+        if (i < n) {
+            spaces--;
+            stars += 2;
+        } else {
+            spaces++;
+            stars -= 2;
+        }
+    }
+
+#endif /* if 0 */
+
+    return 0;
+}
